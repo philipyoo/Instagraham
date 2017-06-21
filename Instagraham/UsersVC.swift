8acbaadc570d6e378503c2340e5176a94a9ba2e6
Philip Yoo <philipyoo10@gmail.com>
2017-06-20 18:52:55 -0700

    Add third tab Users

---
+++ Instagraham/UsersVC.swift
@@ -0,0 +1,35 @@
+//
+//  UsersVC.swift
+//  Instagraham
+//
+//  Created by PHILIP YOO on 6/20/17.
+//
+//
+
+import UIKit
+
+class UsersVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
